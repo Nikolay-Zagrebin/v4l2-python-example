11c81be97ba72e53d303408785013769b779074f
Nikolay-Zagrebin <66779368+Nikolay-Zagrebin@users.noreply.github.com>
2021-03-03 09:39:58 +0300

    Update PIL_display.py

--- fin/PIL_display.py
+++ fin/PIL_display.py
@@ -73,7 +73,12 @@ if __name__ == "__main__":
     read_task = cam.read()
     while True:
         np_data = next(read_task)
+
         PIL_image = Image.fromarray(np_data).resize((356//2, 292//2)).crop((0, 0, 178, 128))
+        #fn = lambda x : 255 if x > 128 else 0
+        #PIL_image = PIL_image.convert('L').point(fn, mode='1')
+        PIL_image = PIL_image.convert('1')
+
         lcd.image.paste(PIL_image, (0,0))
         lcd.update()
         sleep(0.1)
